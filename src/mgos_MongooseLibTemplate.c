03e127a3433ccff762c0720eb622bc4e4a0ac53e
Chris Maria <christopher.maria.109@gmail.com>
2019-03-05 20:06:50 -0500

    Fixed boolean datatype bug.

--- src/mgos_MongooseLibTemplate.c
+++ src/mgos_MongooseLibTemplate.c
@@ -1,3 +1,7 @@
+// the init function requires a boolean return value,
+// but booleans are not included as a default datatype:
+#include <stdbool.h>
+
 // init function is required
 bool mgos_MongooseLibTemplate_init(void) {
     return true;
